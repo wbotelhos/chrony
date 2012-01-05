293d66c0460c983ecbd3d24da049faf91294d349
Washington Botelho <wbotelhos@gmail.com>
2012-01-05 15:12:49 -0200

    improving extend to avoid manual validation

--- js/jquery.chrony.js
+++ js/jquery.chrony.js
@@ -27,7 +27,7 @@
 			return this.each(function() {
 
 				var $this		= $(this),
-					opt			= $.extend({}, $.fn.chrony.defaults, options),
+					opt			= $.extend(true, {}, $.fn.chrony.defaults, options),
 					id			= $this.data('options', opt).attr('id'),
 					separator	= '<span style="float: left;">:</span>';
 
@@ -84,24 +84,6 @@
 				$this.append($hour).append(separator).append($minute).append(separator).append($second);
 
 				var $separators = $this.children('span');
-
-				if (opt.alert) {
-					if (!opt.alert.color) {
-						opt.alert.color = '#F00';
-					}
-
-					if (!opt.alert.hour) {
-						opt.alert.hour = 0;
-					}
-
-					if (!opt.alert.minute) {
-						opt.alert.minute = 0;
-					}
-
-					if (!opt.alert.second) {
-						opt.alert.second = 0;
-					}
-				}
 
 				methods.checkAlert.apply($this, [opt.alert, hour, minute, second]);
 
